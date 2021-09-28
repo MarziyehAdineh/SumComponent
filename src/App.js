a8a6be703605541f8dd3b3ae92e24f7efc6a40ff
marziyehadineh <MarziyehAdineh1.gmail.com>
2021-09-28 16:29:27 +0330

    count edit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from "react";
 import MainComponent from "./MainComponent";
 function NormalizeNum(Number) {
-  return Number
-    .replace("۰","0")
-    .replace("۱","1")
-    .replace("۲","2")
-    .replace("۳","3")
-    .replace("۴","4")
-    .replace("۵","5")
-    .replace("۶","6")
-    .replace("۷","7")
-    .replace("۸","8")
-    .replace("۹","9");
+  return Number.replace("۰", "0")
+    .replace("۱", "1")
+    .replace("۲", "2")
+    .replace("۳", "3")
+    .replace("۴", "4")
+    .replace("۵", "5")
+    .replace("۶", "6")
+    .replace("۷", "7")
+    .replace("۸", "8")
+    .replace("۹", "9");
 }
 const maxValue = 10;
 export default class App extends Component {
   state = { tedad: 3, errorMessage: "", MainComponent: [] };
   handleChange = (e) => {
-    let n1=e.target.value;
-    if (n1 === "" || isNaN(n1)) {
-      this.setState({ tedad: 0, errorMessage: "عدد باید وارد بشود." });
-      return;
-    }
-
+    let n1 = e.target.value;
     let result = NormalizeNum(n1);
     let n2 = parseInt(result);
-
+    if (n2 === "" || isNaN(n2)) {
+      if (n1 === "" || isNaN(n1)) {
+        this.setState({ tedad: 0, errorMessage: "عدد باید وارد بشود." });
+        return;
+      }
+    }
 
     if (n2 > maxValue || n2 < 0) {
       this.setState({
